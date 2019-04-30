fe6bc3168ab4644bc3a22a5c462e6defb9dd7e88
ViktorVx <victorptrv@yandex.ru>
2019-05-01 00:05:57 +0300

    Add new fields to Person

--- src/main/java/org/pva/hibernateChatBot/person/Person.java
+++ src/main/java/org/pva/hibernateChatBot/person/Person.java
@@ -27,9 +27,41 @@ public class Person {
     @Column
     Gender gender;
 
+    @Column
+    String login;
+
+    @Column
+    String password;
+
+    @Column
+    String email;
+
     @ElementCollection
     List<String> phones = new ArrayList<String>();
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public List<String> getPhones() {
         return phones;
