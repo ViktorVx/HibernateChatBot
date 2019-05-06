7c61f7f74814c262814d27702b2bf99f14c87d6e
ViktorVx <victorptrv@yandex.ru>
2019-05-06 12:20:15 +0300

    Begin create simple reply on message

--- src/main/java/org/pva/hibernateChatBot/telegramBot/ResponseHandler.java
+++ src/main/java/org/pva/hibernateChatBot/telegramBot/ResponseHandler.java
@@ -1,7 +1,6 @@
 package org.pva.hibernateChatBot.telegramBot;
 
 import org.pva.hibernateChatBot.person.Person;
-import org.pva.hibernateChatBot.reminder.SimpleReminder;
 import org.telegram.abilitybots.api.db.DBContext;
 import org.telegram.abilitybots.api.sender.MessageSender;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -24,44 +23,16 @@ public class ResponseHandler {
         countMap = db.getMap("COUNTERS");
     }
 
-    @Deprecated
-    public void replyToEnterLogin(long chatId) {
-        try {
-            sender.execute(new SendMessage()
-                    .setText("Введите Ваш уникальный логин:")
-                    .setChatId(chatId)
-                    .setReplyMarkup(KeyboardFactory.getStartCountKeyboard()));
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void replyToEnterReminderDate(long chatId, SimpleReminder simpleReminder) {
-        try {
-            sender.execute(new SendMessage()
-                    .setText("Когда нужно напомнить (дата в формате дд.ММ.гггг)?")
-                    .setChatId(chatId)
-                    .setReplyMarkup(KeyboardFactory.getForceReplyKeyboard()));
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    @Deprecated
-    public void replyToCount(long chatId) {
-        try {
-            Integer counter = countMap.compute(String.valueOf(sender.getMe().getId()), (id, count) -> count == null ? 1 : ++count);
-            String message = String.format("Your count is now *%d*!", counter);
-
-            sender.execute(new SendMessage()
-                    .setText(message)
-                    .setChatId(chatId)
-                    .setReplyMarkup(KeyboardFactory.getStartCountKeyboard()));
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
+//    public void replyToEnterReminderDate(long chatId, SimpleReminder simpleReminder) {
+//        try {
+//            sender.execute(new SendMessage()
+//                    .setText("Когда нужно напомнить (дата в формате дд.ММ.гггг)?")
+//                    .setChatId(chatId)
+//                    .setReplyMarkup(KeyboardFactory.getForceReplyKeyboard()));
+//        } catch (TelegramApiException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     public void replyToStart(long chatId, User user) throws TelegramApiException {
         String MESSAGE = String.format("Привет, %s!\n" +
@@ -98,6 +69,7 @@ public class ResponseHandler {
         String MESSAGE =
                 "Доступны следующие команды:\n" +
                         "/start - начало работы\n" +
+                        "/info - информация о пользователе\n" +
                         "/help - помощь\n" +
                         "/addsimplereminder - добавить простое напоминание\n" +
                         "/viewreminders - показать напоминания";
@@ -110,20 +82,38 @@ public class ResponseHandler {
         }
     }
 
+    //*** Reply to buttons *********************************************************************************************
+
     public void replyToButtons(long chatId, User user, String buttonId, Update upd, Person person) throws TelegramApiException {
         switch (buttonId) {
-            case "start":
-                replyToStart(chatId, user);
-                break;
-            case "count":
-                replyToCount(chatId);
-                break;
+//            case "start":
+//                replyToStart(chatId, user);
+//                break;
+//            case "count":
+//                replyToCount(chatId);
+//                break;
             case "edit_person_data":
                 replyToEditPersonalData(chatId, user, upd, person);
                 break;
             case "edit_register_name":
                 System.out.println("register");
                 break;
+            case "edit_last_name":
+                replyToEditLastName(chatId, person);
+                break;
+        }
+    }
+
+    public void replyToEditLastName(long chatId, Person person) {
+        String MESSAGE =
+                "Введите фамилию:";
+        try {
+            sender.execute(new SendMessage()
+                    .setText(MESSAGE)
+                    .setChatId(chatId).setReplyMarkup(KeyboardFactory.getForceReplyKeyboard()));
+
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 
@@ -135,9 +125,9 @@ public class ResponseHandler {
 
             StringBuilder msg = new StringBuilder();
             msg.append("ℹ️Персональная информация:\n");
-            msg.append(String.format("Фамилия: %s\n", person.getLastName()));
-            msg.append(String.format("Имя: %s\n", person.getFirstName()));
-            msg.append(String.format("Отчество: %s\n", person.getMiddleName()));
+            msg.append(String.format("Фамилия: %s\n", person.getLastName() == null ? "-" : person.getLastName()));
+            msg.append(String.format("Имя: %s\n", person.getFirstName() == null ? "-" : person.getFirstName()));
+            msg.append(String.format("Отчество: %s\n", person.getMiddleName() == null ? "-" : person.getMiddleName()));
 
             EditMessageText new_message = new EditMessageText().
                     setChatId(chat_id).
@@ -150,6 +140,21 @@ public class ResponseHandler {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    @Deprecated
+    public void replyToCount(long chatId) {
+        try {
+            Integer counter = countMap.compute(String.valueOf(sender.getMe().getId()), (id, count) -> count == null ? 1 : ++count);
+            String message = String.format("Your count is now *%d*!", counter);
+
+            sender.execute(new SendMessage()
+                    .setText(message)
+                    .setChatId(chatId)
+                    .setReplyMarkup(KeyboardFactory.getStartCountKeyboard()));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 }
